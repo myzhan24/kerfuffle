d920c69018acef39bd2d41f23ddccb3822d81c8f
myzhan24 <myzhan24@gmail.com>
2019-04-28 10:59:00 -0500

    tweaking movement and jumping

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,6 +1,5 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-
 import Mushroom from '../sprites/Mushroom';
 import Player from '../sprites/Player';
 import Platform from '../sprites/Platform';
@@ -12,9 +11,41 @@ export default class extends Phaser.Scene {
     }
 
     init() {
+        this.initKeyBinds();
     }
 
     preload() {
+    }
+
+    initKeyBinds() {
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    updateKeyBinds() {
+        if (this.keyA.isDown) {
+            this.player.vectorY = 11;
+        }
+
+        if (this.keyLeft.isDown && this.player.vectorX > -10) {
+            this.player.accelX = -1;
+            if (this.player.vectorX == 0) {
+                this.player.setVectorX(-6);
+            } else if (this.player.vectorX > 0) {
+                this.player.setVectorX(-3);
+            }
+        } else if (this.keyRight.isDown && this.player.vectorX < 10) {
+            this.player.accelX = 1;
+            if (this.player.vectorX == 0) {
+                this.player.setVectorX(6);
+            } else if (this.player.vectorX < 0) {
+                this.player.setVectorX(3)
+            }
+        } else {
+            this.player.accelX = 0;
+            // this.player.decayVectorX();
+        }
     }
 
     create() {
@@ -57,17 +88,29 @@ export default class extends Phaser.Scene {
     }
 
     update() {
+        this.updateKeyBinds();
         // If a universe Object intersects with a platform, reset its y and vector y
         for (let uniObj of this.universe.getChildren()) {
             for (let platform of this.platforms.getChildren()) {
                 if (overlaps(uniObj, platform)) {
                     uniObj.y = platform.y - uniObj.height / 2;
-                    uniObj.vectorY = 0;
 
+                    if (uniObj.vectorY < 0) {
+                        uniObj.vectorY = 0;
+                    }
                     // Assume this object can only overlap with one platform at a time.
+
+                    if (!this.keyLeft.isDown && !this.keyRight.isDown) {
+                        uniObj.decayVectorX();
+                    }
+
+
                     break;
                 }
             }
         }
+
+
+        console.log('player', this.player.vectorX);
     }
 }
--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -6,7 +6,7 @@ export default class extends Phaser.GameObjects.Sprite {
         this.vectorX = 0;
         this.vectorY = 0;
         this.accelX = 0;
-        this.accelY = -0.4;
+        this.accelY = -0.8;
     }
 
     update() {
@@ -14,5 +14,49 @@ export default class extends Phaser.GameObjects.Sprite {
         this.vectorY += this.accelY;
         this.x += this.vectorX;
         this.y -= this.vectorY;
+    }
+
+
+    decayVectorX() {
+
+        if (this.getVectorX() < 0) {
+            this.setAccelX(1);
+        } else if (this.getVectorX() > 0) {
+            this.setAccelX(-1);
+        }
+
+        if (Math.abs(this.getVectorX()) <= 0.5) {
+            this.setAccelX(0);
+            this.setVectorX(0);
+        }
+        console.log('mz decay', this.getVectorX(), this.accelX);
+    }
+
+    /*decayVectorX() {
+        this.setVectorX(this.getVectorX() / 1.05);
+    }*/
+
+    setVectorX(vector) {
+        this.vectorX = vector;
+    }
+
+    getVectorX() {
+        return this.vectorX;
+    }
+
+    adjustVectorX(value) {
+        this.vectorX += value;
+    }
+
+    setVectorY(vector) {
+        this.vectorY = vector;
+    }
+
+    setAccelX(vector) {
+        this.accelX = vector;
+    }
+
+    setAccelY(vector) {
+        this.accelY = vector;
     }
 }
