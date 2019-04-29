f9892b04089e76820202a7fe8c9c259c722fa2fa
myzhan24 <myzhan24@gmail.com>
2019-04-29 13:47:02 -0500

    added some docs

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,9 +1,9 @@
 export const Physics = {
     gravity: -0.8, // Y Acceleration downward
-    airAccelMu: 0.5, // X Acceleration in the Air
-    groundAccelMu: 1, // X Acceleration on the Ground
-    airFrictionMu: 0.25, // X Deceleration in the Air
-    groundFrictionMu: 2 // X Deceleration on the Ground
+    airAccelMu: 0.5, // X Acceleration multiplier in the Air
+    groundAccelMu: 1, // X Acceleration multiplier on the Ground
+    airFrictionMu: 0.25, // X Deceleration multiplier in the Air
+    groundFrictionMu: 2 // X Deceleration multiplier on the Ground
 }
 
 export const Player = {
